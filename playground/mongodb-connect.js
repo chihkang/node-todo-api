43e25d28016c4c6e8be98f5adb6438b9cb94235d
ChihKang <chihkang@me.com>
2017-02-16 22:33:58 +0800

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+// var user={name:'Chihkang',age:25};
+// var {name} = user;
+// console.log(name);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unablt to connect to MongoDB server');
+  }
+  console.log('Connect to MongoDB server');
+
+// db.collection('Todos').insertOne({
+//   text:'Something to do',
+//   completed:false
+// },(err,result)=>{
+//   if(err){
+//     return console.log('Unable to insert todd',err);
+//   }
+//   console.log(JSON.stringify(result.ops,undefined,2));
+// });
+
+// Insert new doc into Users(name,age,location)
+// db.collection('Users').insertOne({
+//   name:'Chihkang',
+//   age:30,
+//   location:'Taipei'
+// },(err,result)=>{
+//   if(err){
+//     return console.log('Unable to insert user',err);
+//   }
+//   console.log(JSON.stringify(result.ops,undefined,2));
+//   console.log(result.ops[0]._id.getTimestamp());
+// });
+
+  db.close();
+});
